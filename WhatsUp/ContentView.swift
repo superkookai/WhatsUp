bbf8b098b65dd1619539e96154faecb07620f3f3
superkookai <weerawut@live.com>
2025-03-24 10:47:41 +0700

    Initial Commit

---
+++ WhatsUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsUp
+//
+//  Created by Weerawut Chaiyasomboon on 24/03/2568.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsUp/WhatsUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsUpApp.swift
+//  WhatsUp
+//
+//  Created by Weerawut Chaiyasomboon on 24/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct WhatsUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
